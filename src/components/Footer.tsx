9132a990e0cae9b854ca06d4b03a4ee67599a93e
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2023-04-25 11:48:02 -0400

    adding logos to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,20 @@
 import '../styles/Footer.css'
 import twitter from '../assets/twitter.png'
+import react from '../assets/react_logo.png'
+import typeScript from '../assets/type_script_logo.png'
+
 export const Footer = (): JSX.Element => {
   return (
     <footer className='footer'>
-      <h4>
-        React+Ts ⚛️
+      <h5>
+        React &nbsp;<img src={react} alt='React logo'/>&nbsp;+&nbsp;<img src={typeScript} alt="TypeScript logo" /> &nbsp;
         <span>
           <a href='https://www.github.com/OcroDev'>@OcroDev</a>
         </span>
-      </h4>
+      </h5>
       <div>
       </div>
-      <h5><b style={{ color: '#00acee' }} > Twitter <img src={twitter} alt="twitter logo"/> </b> &nbsp; follow card ( clone )</h5>
+      <h6><b style={{ color: '#00acee' }} > Twitter <img src={twitter} alt="twitter logo"/> </b> &nbsp; follow card ( clone )</h6>
     </footer>
   )
 }
